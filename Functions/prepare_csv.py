448420b79bf7fd43eebe042b776d4c89e80c2ef0
Lukas Leindals <46963040+s183920@users.noreply.github.com>
2020-07-02 16:35:11 +0200

    mix song order

--- Functions/prepare_csv.py
+++ Functions/prepare_csv.py
@@ -12,7 +12,7 @@ def create_song_csv(file_name, n_songs = 100, song_sheet = "Sange", csv_name = "
     n_songs = how many songs to use (fill be taken from start of the file) \n
     song_sheet = if the file is an .xslx file, the name of the sheet containing the songs must be given here \n
     csv_name = name of the output csv, must have the .csv extension \n
-    diff_song_length = whether the songs have varying/different lengths \n
+    diff_song_length = whether the songs have varying/different lengths, must have the column "sluttidspunkt (i sek)" to do so \n
     """
     print("Creating the song csv...")
 
@@ -25,7 +25,7 @@ def create_song_csv(file_name, n_songs = 100, song_sheet = "Sange", csv_name = "
         raise AssertionError("Please use a valid file extension (xlsx or csv)")
 
     # create the csv 
-    cols = ["Sang - Kunstner", "link", "starttidspunkt (i sek)", "sluttidspunkt (i sek)", "Shoutout"] if diff_song_length else ["Sang - Kunstner", "link", "starttidspunkt (i sek)", "Shoutout"]
+    cols = ["Sang - Kunstner", "link", "starttidspunkt (i sek)", "sluttidspunkt (i sek)", "Shoutout", "behold placering"] if diff_song_length else ["Sang - Kunstner", "link", "starttidspunkt (i sek)", "Shoutout", "behold placering"]
 
     if file_extension == ".xlsx":
         songs = pd.read_excel(file_name, sheet_name=song_sheet, usecols = cols).dropna(how = "all")
@@ -35,7 +35,7 @@ def create_song_csv(file_name, n_songs = 100, song_sheet = "Sange", csv_name = "
     if diff_song_length:
         songs["Song length"] = songs["sluttidspunkt (i sek)"]-songs["starttidspunkt (i sek)"]
         songs["Song length"] = songs["Song length"].fillna(60)
-        songs = songs.loc[:, ["Sang - Kunstner", "link", "starttidspunkt (i sek)", "Song length", "Shoutout"]]
+        songs = songs.loc[:, ["Sang - Kunstner", "link", "starttidspunkt (i sek)", "Song length", "Shoutout", "behold placering"]]
 
     songs.iloc[:n_songs, :].to_csv(csv_name, index = False, header = False)
 
@@ -69,6 +69,7 @@ def create_shoutout_csv(file_name, n_shoutouts = 100, shoutout_sheet = "Shoutout
     shoutouts["SO length"] = shoutouts["SO length"].fillna(max_length)
     shoutouts.iloc[:n_shoutouts, [0, 1, 2, 4]].to_csv(csv_name, index = False, header = False)
 
+
 def get_trim_vals(csv = "Shoutouts.csv"):
     """
     Gets the values to use for trimming the shoutouts, returns a data frame with the starts times in the first column and the the length as the second.
@@ -81,8 +82,40 @@ def get_trim_vals(csv = "Shoutouts.csv"):
     lens.columns = list(range(2))
     return lens
 
-# def arrange_shoutout_csv(song_csv = "Songs.csv", shoutout_csv = "Shoutouts.csv"):
-#     song_so = pd.read_csv(song_csv, usecols=)
+def mix_pos(csv = "Songs.csv", diff_song_length = False):
+    """
+    Mixes up the order of the songs or shoutouts
+    ---------------------------------------
+    csv: a csv in the format of create_song_csv/shoutout_csv, where a character in the column "behold placering", keeps the song in its original position \n
+    diff_song_length: whether the song_csv contains the column "sluttidspunkt (i sek)" or not
+    """
+    df = pd.read_csv(csv, usecols = [5]) if diff_song_length else pd.read_csv(csv, usecols = [4], header = None, squeeze = True)
+    l = len(df)
+    mix_loc = list(*np.where(pd.isnull(df)))
+    keep_loc = [i for i in range(l) if i not in mix_loc]
+    order = []
+    perm_mix = np.random.permutation(mix_loc)
+    for i in range(l):
+        if i in keep_loc:
+            order.append(i)
+        else:
+            order.append(perm_mix[0])
+            perm_mix = np.delete(perm_mix, 0)
+    
+    pd.read_csv(csv, header = None).reindex(order).to_csv(csv, index = False, header = False)
+
+
+# def arrange_shoutout_csv(song_csv = "Songs.csv", shoutout_csv = "Shoutouts.csv", diff_song_length = False):
+#     """
+#     Arranges the shoutout csv due to the placement in the song csv
+#     """
+#     song_so = pd.read_csv(song_csv, usecols = [4]) if diff_song_length else pd.read_csv(song_csv, usecols = [3], header = None, squeeze = True)
+#     shoutout_so = pd.read_csv(shoutout_csv, usecols = [0], header = None, squeeze = True)
+
+#     print(song_so in shoutout)
+
+
+#     print(song_so, shoutout_so)
 
 
 if __name__ == "__main__":
@@ -96,6 +129,9 @@ if __name__ == "__main__":
     # create_song_csv("Examples/Børne Klub 100/Børne Klub 100.xlsx")
     # print(get_club_len("Examples/Børne Klub 100/Børne Klub 100.xlsx"))
    
-    create_song_csv("Examples/Børne Klub 100/test_kid2.xlsx", csv_name="test_songs.csv", diff_song_length=True, n_songs=4)
-    create_shoutout_csv("Examples/Børne Klub 100/test_kid2.xlsx", csv_name="test_so.csv") 
-    print(get_trim_vals("test_songs.csv"))
+    # create_song_csv("template.xlsx", csv_name="test_songs.csv", diff_song_length=False, n_songs=14)
+    # create_shoutout_csv("template.xlsx", csv_name="test_so.csv", n_shoutouts=14) 
+    # print(get_trim_vals("test_songs.csv"))
+    mix_song_pos("test_songs.csv", diff_song_length = False)
+    # arrange_shoutout_csv(song_csv = "test_songs.csv", shoutout_csv="test_so.csv")
+    # arrange_shoutout_csv(song_csv = "test_songs2.csv", shoutout_csv="test_so2.csv")
